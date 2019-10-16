2af427ff3284126c3d32233067be65896999e425
Vina del Mar <vina@duoc.acad>
2019-10-16 12:36:37 -0300

    cambios a santaTeresita

--- santaTeresita/views.py
+++ santaTeresita/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'formulario/post_list.html', {'posts': posts})
+    return render(request, 'santaTeresita/post_list.html', {'posts': posts})
